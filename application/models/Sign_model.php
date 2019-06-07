4ed71d68c007d751be6b2cd35d4b134c444d29e9
kataynoi <thait-rex@hotmail.com>
2019-06-07 10:03:35 +0700

    editSign

--- application/models/Sign_model.php
+++ application/models/Sign_model.php
@@ -13,19 +13,28 @@ class Sign_model extends CI_Model
     public function get_sign($data){
         $month = $data['year'].'-'.$data['month'];
         $workgroup = $data['workgroup'];
-        $sql = "SELECT * FROM (SELECT
+         $sql = "SELECT * FROM (SELECT
                         a.user_id,CONCAT(b.prename,b.`name`) as `name`
+
+                        ,GROUP_CONCAT(CONCAT(DATE_FORMAT(date_work,'%d'),'|',sign_in,'|',sign_out,'|',c.s_type)) as date_work1
                         ,GROUP_CONCAT(date_work) as date_work
                         ,GROUP_CONCAT(sign_in) as sign_in
                         ,GROUP_CONCAT(sign_out) as sign_out
                         ,GROUP_CONCAT(c.s_type) as sign_type
                         ,b.order as member_order
-                        FROM (SELECT * FROM sign_work WHERE DATE_FORMAT(date_work,'%Y-%m') = '".$month."' ORDER BY date_work) a
+                        FROM (SELECT * FROM sign_work WHERE DATE_FORMAT(date_work,'%Y-%m') = '".$month."' ORDER BY date_work ASC) a
                         LEFT JOIN mas_users b ON a.user_id = b.id
                         LEFT JOIN sign_type c ON a.sign_type = c.id
                         WHERE b.`group`='".$workgroup."'
                         GROUP BY a.user_id) a ORDER BY a.member_order";
         $rs=$this->db->query($sql)->result();
+
+        /*$rs= $this->db
+            ->select('a.id, a.prename, a.name, a.position')
+            ->where('a.group',$workgroup)
+            ->where('a.movedate','0000-00-00 00:00:00')
+            ->get('mas_users a')
+            ->result();*/
         return $rs;
 
     }
--- assets/apps/js/sign.js
+++ assets/apps/js/sign.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $("#btn_login").removeAttr("disabled");
     //User namespace
     var sign = {};
+    var day_of_month=0;
     sign.ajax = {
         get_sign: function (items, cb) {
             var url = '/signin/get_sign',
@@ -24,60 +25,87 @@ $(document).ready(function(){
             else {
                 //data = JSON.parse(data);
                 console.log(data.success);
-                if(data.success){
-                    //swal('Get Success');
-                    var no=1;
-                    $('#tbl_list > tbody').empty();
-                    if (_.size(data.rows) > 0) {
-                        _.each(data.rows, function (v) {
-                            //v.sign_in = v.sign_in.replace('00:00:00','-');
-                            var signin = v.sign_in.split(",");
-                            var signout = v.sign_out.split(",");
-                            var signtype = v.sign_type.split(",");
-                            var signin_txt='',signout_txt='',signtype_txt='';
-                            for(i=0;i<signin.length;i++){
-                                signin_txt +='<td style="font-size:10px;padding: 1px;margin: 2px">'+signin[i].substr(0,5)+'</td>';
-                            }
-                            signin_txt ='<td style="font-size:10px;padding: 1px;margin: 2px">เวลามา</td>'+signin_txt+'</tr>';
-                            signin_txt = signin_txt.replace(/00:00/g,'');
-
-                            for(i=0;i<signout.length;i++){
-                                signout_txt +='<td style="font-size:10px;padding: 1px;margin: 2px">'+signout[i].substr(0,5)+'</td>';
-                            }
-                            signout_txt ='<td style="font-size:10px;padding: 1px;margin: 2px">เวลากลับ</td>'+signout_txt+'</tr>';
-                            signout_txt = signout_txt.replace(/00:00/g,'');
-
-                            for(i=0;i<signtype.length;i++){
-                                signtype_txt +='<td style="font-size:10px;padding: 1px;margin: 2px;text-align: center;color: #003399">'+signtype[i]+'</td>';
-                            }
-                            signtype_txt ='<td style="font-size:10px;padding: 1px;margin: 2px">หมายเหตุ</td>'+signtype_txt+'</tr>';
-
-
-
-                            $('#tbl_list > tbody').append(
-                                '<tr>' +
-                                '<td class="row" rowspan="3">'+no+'<span style="display: none" ><i class="fa fa-check text-success"></i></span></td>' +
-                                '<td rowspan="3" >' + v.name + '</td>' +
-                                ''+signin_txt+signout_txt+signtype_txt+
-                                '</tr>'
-                            );
-                            no++;
-                        });
-                    }
-                    else {
-                        $('#tbl_list > tbody').append('<tr><td colspan="32">ไม่พบรายการ</td></tr>');
-                    }
-                }else{
-                   swal('ไม่พบข้อมูล');
-                }
+                sign.set_sign(data);
             }
         });
+
+
+    }
+    sign.set_sign = function(data){
+        //data = JSON.parse(data);
+        console.log(data.success);
+        if(data.success){
+            //swal('Get Success');
+            var no=1;
+            $('#tbl_list > tbody').empty();
+            if (_.size(data.rows) > 0) {
+                _.each(data.rows, function (v) {
+                    //v.sign_in = v.sign_in.replace('00:00:00','-');
+
+                    var sign_work = v.date_work1.split(",");
+
+                    var signin_txt='';
+
+                    var txt_day='';
+                    for(i=1;i<=day_of_month;i++){
+                        txt_day +='<td style="font-size:10px;padding: 1px;margin: 2px;text-align:center;" data-day=U'+v.user_id+'D'+i+'>'+'x'+'</td>';
+                        //console.log(txt_day);
+                    }
+
+                    $('#tbl_list > tbody').append(
+                        '<tr data-id='+v.user_id+'>' +
+                        '<td class="row">'+no+'</td>' +
+                        '<td >' + v.name + '</td>' +
+                        '<td style="font-size:10px;padding: 1px;margin: 2px">เวลามา<br>เวลากลับ<br>หมายเหตุ</td>' +
+                        txt_day+
+                        '</tr>'
+                    );
+                    for(i=0;i<sign_work.length;i++){
+                        sign_work[i] = sign_work[i].replace(/00:00:00/g,'');
+                        sign_work[i] = sign_work[i].replace(/:00/g,'');
+                        sign_work[i]= sign_work[i].split("|");
+                        signin_txt +='<div>'+sign_work[i][0]+'</div>';
+                        //var td_target=$("tr").data("id")== ;
+                        var id='U'+v.user_id+'D'+parseInt(sign_work[i][0]);
+                        var target_td = $('td[data-day="'+id+'"]');
+                        target_td.html(sign_work[i][1]+'<br>'+sign_work[i][2]+'<br>'+sign_work[i][3]);
+                        //$('#tbl_list > tbody').append();
+                    }
+
+
+                    /*for(i=0;i<signin.length;i++){
+                        signin_txt +='<td style="font-size:10px;padding: 1px;margin: 2px">'+signin[i].substr(0,5)+'</td>';
+                    }
+                    signin_txt ='<td style="font-size:10px;padding: 1px;margin: 2px">เวลามา</td>'+signin_txt+'</tr>';
+                    signin_txt = signin_txt.replace(/00:00/g,'');
+
+                    for(i=0;i<signout.length;i++){
+                        signout_txt +='<td style="font-size:10px;padding: 1px;margin: 2px">'+signout[i].substr(0,5)+'</td>';
+                    }
+                    signout_txt ='<td style="font-size:10px;padding: 1px;margin: 2px">เวลากลับ</td>'+signout_txt+'</tr>';
+                    signout_txt = signout_txt.replace(/00:00/g,'');
+
+                    for(i=0;i<signtype.length;i++){
+                        signtype_txt +='<td style="font-size:10px;padding: 1px;margin: 2px;text-align: center;color: #003399">'+signtype[i]+'</td>';
+                    }
+                    signtype_txt ='<td style="font-size:10px;padding: 1px;margin: 2px">หมายเหตุ</td>'+signtype_txt+'</tr>';
+*/
+
+                    no++;
+                });
+            }
+            else {
+                $('#tbl_list > tbody').append('<tr><td colspan="32">ไม่พบรายการ</td></tr>');
+            }
+        }else{
+            swal('ไม่พบข้อมูล');
+        }
     }
 
     $('#get_sign').on('click',function(e){
         e.preventDefault();
         var items={};
-        var day_of_month=0;
+
         items.year=$('#sl_year').val();
         items.month = $('#sl_month').val();
         items.workgroup=$('#sl_workgroup').val();
